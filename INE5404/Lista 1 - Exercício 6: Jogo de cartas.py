9e41a96857dbb3b6ad6bff51128af12a29f8df74
matheus <matheus.reinert@protonmail.com>
2020-03-13 12:57:58 -0300

    Continue later

--- INE5404/Lista 1 - Exercício 6: Jogo de cartas.py
+++ INE5404/Lista 1 - Exercício 6: Jogo de cartas.py
@@ -1,1 +1,25 @@
+from random import randint
 
+
+
+class Carta:
+
+    def __init__(self, titulo, tipo, descricao):
+        self.nome = titulo
+        self.tipo = tipo
+        self.descricao = descricao
+
+
+
+class CartaMagica(Carta):
+    
+    pass
+
+class CartaArmadilha(Carta):
+
+    pass
+ 
+
+pog = CartaMagica("Pote da ganância", "Magia Normal", "?")
+
+print(pog.nome, pog.descricao)
