f4570f188306bad785fe6bf778e3fbb180d28893
Matheus Filipe dos Santos Reinert <62085690+shinobu-uwu@users.noreply.github.com>
2020-03-13 16:10:21 -0300

    Update Lista 1 - Exercício 6: Jogo de cartas.py
    To be continued

--- INE5404/Lista 1 - Exercício 6: Jogo de cartas.py
+++ INE5404/Lista 1 - Exercício 6: Jogo de cartas.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, shuffle
 
 
 
@@ -15,11 +15,48 @@ class CartaMagica(Carta):
     
     pass
 
+
+
 class CartaArmadilha(Carta):
 
     pass
- 
 
+
+
+class CartaMonstro(Carta):
+    
+    def __init__(self, titulo, tipo, descricao, atributo, nivel, raca):
+        super().__init__(titulo, tipo, descricao)
+        self.atributo = atributo
+        self.nivel = nivel
+        self.raca = raca
+        
+
+
+class Baralho:
+    
+    def __init__(self, cartas):
+        self.cartas = cartas
+        
+        
+        
+    def embaralhar(self):
+        shuffle(self.cartas)           
+                    
+    
+    
+    def sacar(self):
+        self.cartas.pop(0)
+
+
+
+dm = CartaMonstro("Mago Negro", "Monstro Normal", "O mago definitivo em termos de ataque e defesa", "Trevas", 7, "Mago")
 pog = CartaMagica("Pote da ganância", "Magia Normal", "?")
+bewd = CartaMonstro("Dragão branco de olhos azuis", "Monstro Normal" ,"Descrição muito grande", "Luz", 8, "Dragão")
 
-print(pog.nome, pog.descricao)
+deck = Baralho([dm, pog, bewd])
+deck.embaralhar()
+deck.sacar()
+for i in range(len(deck.cartas)):
+    print(deck.cartas[i].nome)
+    
