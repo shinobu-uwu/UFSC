4f22362e63e51c6eedaa3aea77d267d677bce825
matheus <matheus.reinert@protonmail.com>
2020-03-23 14:55:05 -0300

    Cotinue Warmup

--- INE5404/Lista 1/Warmup.py
+++ INE5404/Lista 1/Warmup.py
@@ -10,3 +10,24 @@ class Televisao:
 
         self.tamanho = tamanho
         self.marca = marca
+
+    #parte 3:
+
+        def muda_canal_para_cima():
+            self.canal += 1
+
+        def muda_canal_para_baixo():
+            self.canal -= 1
+
+
+
+#criando objetos para parte 2:
+
+tv1 = Televisao(21, "Philips")
+tv2 = Televisao(24, "Toshiba")
+print(f"""
+Tamanho televisão 1: {tv1.tamanho}
+Marca televisão 1: {tv1.marca}
+Tamanho televisão 2: {tv2.tamanho}
+Marca televisão 2: {tv2.marca}
+""")
