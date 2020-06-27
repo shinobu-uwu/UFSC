763aac75408c876c7a73706cf56033bc6b75134d
Matheus <matheus.reinert@protonmail.com>
2020-06-27 00:15:06 -0300

    Done

--- INE5404/Lista 1/Exercício 1: Banco.py
+++ INE5404/Lista 1/Exercício 1: Banco.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Cliente:
 
     def __init__(self, nome, telefone):
@@ -5,67 +8,62 @@ class Cliente:
         self.telefone = telefone
 
 
+class ContaCorrente: #Como o banco tatu quer controlar o saldo dos seus correntistas, toda conta que criaremos será uma conta corrente
 
-class Conta:
-
-    def __init__(self, clientes : []):
-        self.titulares = clientes
+    def __init__(self, clientes): #Os clientes serão dados em uma lista 
+        self.titular = clientes
         self.saldo = 0
         self.historico = list()
+        self.data_criacao = str(datetime.date.today())
 
     def saque(self, valor):
-        if valor <= self.saldo:
+        if self.saldo >= valor:
             self.saldo -= valor
-            self.historico.append(f"Saque de {valor}")
+            self.historico.append({"Data" : datetime.datetime.now(), "Operação" : "Saque", "Valor" : valor})
         else:
             print("Saldo insuficiente")
 
 
     def deposito(self, valor):
         self.saldo += valor
-        self.historico.append(f"Depósito de {valor}")
+        self.historico.append({"Data" : datetime.datetime.now(), "Operação" : "Depósito", "Valor" : valor})
 
 
     def extrato(self):
         print("Extrato da conta: \n")
         for transacao in self.historico:
-            print(transacao)
+            print(f'{transacao["Data"]} - {transacao["Operação"]} de R${transacao["Valor"]}')
         print()
 
-    def resumo_titulares(self):
-        print("Titulares:\n")
-        for cliente in self.titulares:
-            print(cliente.nome + ", telefone " + cliente.telefone)
-        print(f"\nSaldo de {self.saldo}")
-        
+    def resumo(self):
+        print("Titular: \n")
+        for cliente in self.titular:
+            print(f"{cliente.nome}, telefone: {cliente.telefone}")
+        print(f"\nSaldo de R${self.saldo}")
+        print(f"Data de criação: {self.data_criacao[8:10]}/{self.data_criacao[5:7]}/{self.data_criacao[0:4]}")
 
 
+class ContaEspecial(ContaCorrente):
 
-class ContaCorrente(Conta):
-
-    pass
-
-
-
-class ContaEspecial(Conta):
-
-    def __init__(self, clientes : [], limite):
-        super().__init__(clientes)
+    def __init__(self, clientes, limite): 
+        super().__init__(clientes)#Herda o construtor da classe mãe
         self.limite = limite
-
+    
     def saque(self, valor):
-        if valor <= self.saldo + self.limite:
+        if self.saldo + self.limite >= valor :
             self.saldo -= valor
-            self.historico.append(f"Depósito de {valor}")
+            self.historico.append({"Data" : datetime.datetime.now(), "Operação" : "Saque", "Valor" : valor})
         else:
             print("Saldo insuficiente")
 
+class ContaPoupança(ContaCorrente):
 
+    def __init__(self, clientes, rendimento):
+        super().__init__(clientes)
+        self.rendimento = rendimento
 
-
-cliente = Cliente("João", "3123")
-conta = ContaCorrente([cliente])
-conta.deposito(50)
-conta.saque(25)
-conta.extrato()
-conta.resumo_titulares()
+    def deposito(self, valor):
+        hoje = datetime.date.today()
+        ultima_transacao = self.historico[-1]["Data"]
+        meses = (hoje.year - ultima_transacao.year) * 12 + (hoje.month - ultima_transacao.month)
+        super().deposito(valor * self.rendimento * meses)
