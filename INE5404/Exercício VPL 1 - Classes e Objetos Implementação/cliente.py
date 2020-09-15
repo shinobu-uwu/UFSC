151a962cd9435a47621febde03457d8e78ad8e16
Matheus <matheus.reinert@protonmail.com>
2020-09-15 17:47:30 -0300

    ToDo

---
+++ INE5404/Exercício VPL 1 - Classes e Objetos Implementação/cliente.py
@@ -0,0 +1,16 @@
+
+
+class Cliente:
+
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    """Insira aqui os demais metodos ... """
