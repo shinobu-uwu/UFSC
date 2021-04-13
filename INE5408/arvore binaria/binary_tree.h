5a17df69704c41195da8669ee2177bfa7d1207fb
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2021-04-13 13:06:34 -0300

    B

---
+++ INE5408/arvore binaria/binary_tree.h
@@ -0,0 +1,12 @@
+namespace structures {
+    template<typename T>
+    class BinaryTree {
+    private:
+        class Node {
+        public:
+            explicit Node(T Data) {
+
+            }
+        };
+    };
+}
